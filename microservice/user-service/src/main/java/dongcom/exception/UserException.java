1913eeedc6781f9682a6722431115e7fa23cde16
Dong Nguyen <dlttthvlhhth@gmail.com>
2025-03-30 12:25:44 -0700

    initialize study service

--- microservice/user-service/src/main/java/dongcom/exception/UserException.java
+++
@@ -1,8 +0,0 @@
-package dongcom.exception;
-
-public class UserException extends RuntimeException { // Changed from Exception to RuntimeException
-    public UserException(String message) {
-        super(message);
-        System.out.println("This is user Exception");
-    }
-}
--- microservice/user-service/src/main/java/dongcom/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package dongcom.repository;
-
-import dongcom.modal.User;
-import org.springframework.data.jpa.repository.JpaRepository;;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-
-}
