55b3aba0b10e4c7fa941b6922860709d8624f9f8
Dong Nguyen <dlttthvlhhth@gmail.com>
2025-05-10 19:52:02 -0700

    create category modal for category service

---
+++ category-microservice/src/main/java/dongcom/modal/Category.java
@@ -0,0 +1,23 @@
+package dongcom.modal;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+
+@Entity
+@Getter
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long Id;
+
+    @Column(nullable = false)
+    private String name;
+
+    private String image;
+
+    private Long studyId;
+}
