8ec6be82fbc52010816854293c524bbb54f9b89f
Dong Nguyen <dlttthvlhhth@gmail.com>
2025-05-22 21:47:43 -0700

    create serviceDTO, categoyDTO and interface for method updateService

--- service-offering/src/main/java/dongcom/service/ServiceOffieringService.java
+++ service-offering/src/main/java/dongcom/service/ServiceOffieringService.java
@@ -3,7 +3,10 @@ package dongcom.service;
 import dongcom.modal.ServiceOffering;
 import dongcom.payload_dto.StudyDTO;
 import dongcom.payload_dto.CategoryDTO;
+import dongcom.payload_dto.ServiceDTO;
 
 public interface ServiceOffieringService {
-    ServiceOffering createService(StudyDTO studyDTO, CategoryDTO categoryDTO);
+    ServiceOffering createService(StudyDTO studyDTO, ServiceDTO serviceDTO, CategoryDTO categoryDTO);
+
+    ServiceOffering updateService(Long serviceId, ServiceOffering service);
 }
