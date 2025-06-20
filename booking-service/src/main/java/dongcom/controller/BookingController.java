4e044fa631eb4f4785a16433edb3cad813bb11c5
Dong Nguyen <dlttthvlhhth@gmail.com>
2025-06-20 02:01:32 -0700

    finish implement createBooking API endpoint

--- booking-service/src/main/java/dongcom/controller/BookingController.java
+++ booking-service/src/main/java/dongcom/controller/BookingController.java
@@ -1,5 +1,8 @@
 package dongcom.controller;
 
+import java.util.*;
+
+import org.apache.catalina.connector.Response;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,6 +11,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 import dongcom.modal.Booking;
 import dongcom.payload_dto.BookingRequest;
+import dongcom.payload_dto.StudyDTO;
+import dongcom.payload_dto.UserDTO;
+import dongcom.payload_dto.ServiceDTO;
 import dongcom.service.BookingService;
 
 import lombok.RequiredArgsConstructor;
@@ -20,7 +26,24 @@ public class BookingController {
 
     public ResponseEntity<Booking> createBooking(
             @RequestParam Long studyId,
-            @RequestBody BookingRequest bookingRequest) {
-        return null;
+            @RequestBody BookingRequest bookingRequest) throws Exception {
+
+        UserDTO user = new UserDTO();
+        user.setId(1L);
+
+        StudyDTO study = new StudyDTO();
+        study.setId(studyId);
+
+        Set<ServiceDTO> serviceDTOSet = new HashSet<>();
+        ServiceDTO serviceDTO = new ServiceDTO();
+        serviceDTO.setId(1L);
+        serviceDTO.setPrice(399);
+        serviceDTO.setDuration(45);
+        serviceDTO.setName("self study path way guide");
+
+        serviceDTOSet.add(serviceDTO);
+
+        Booking booking = BookingService.createBooking(bookingRequest, user, study, serviceDTOSet);
+        return ResponseEntity.ok(booking);
     }
 }
