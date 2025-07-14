f6e9b156f6c763662f1a8ccf739ebe572f25f347
Dong Nguyen <dlttthvlhhth@gmail.com>
2025-07-14 00:06:41 -0700

    add dependencis razor payment and stripe

--- payment-service/src/main/java/dongcom/service/PaymentService.java
+++ payment-service/src/main/java/dongcom/service/PaymentService.java
@@ -1,10 +1,19 @@
 package dongcom.service;
 
 import dongcom.domain.PaymentMethod;
+import dongcom.modal.PaymentOrder;
 import dongcom.payload.PaymentLinkResponse;
 import dongcom.payload.payload_dto.UserDTO;
 import dongcom.payload.payload_dto.BookingDTO;
 
+import com.razorpay.PaymentLink;
+
 public interface PaymentService {
     PaymentLinkResponse createOrder(UserDTO user, BookingDTO booking, PaymentMethod paymentMethod);
+
+    PaymentOrder getPaymentOrderById(Long id);
+
+    PaymentOrder getPaymentOrderByPaymentId(String paymentId);
+
+    PaymentLink createRazorPaymentLink(UserDTO user, Long amount, Long orderId);
 }
